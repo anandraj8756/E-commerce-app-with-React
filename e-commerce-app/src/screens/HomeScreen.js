fe2ef1a2805cb0875c30ff3a39488b97c2b3398f
Anand Raj <anandallen95@gmail.com>
2021-06-18 11:18:17 +0530

    loading data from backend

--- e-commerce-app/src/screens/HomeScreen.js
+++ e-commerce-app/src/screens/HomeScreen.js
@@ -1,18 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import data from '../data';
+import axios from 'axios';
 import Product from '../components/Product';
+import MessageBox from '../components/MessageBox';
+import LoadingBox from '../components/LoadingBox';
 
 
 
 export default function HomeScreen() {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  useEffect(() => {
+    const fecthData = async () => {
+      try{
+        setLoading(true);
+      const {data} = await axios.get('/api/products');
+      setLoading(false);
+      setProducts(data);
+
+      }catch(err){
+        setError(err.message);
+        setLoading(false);
+      }
+
+      
+    };
+    fecthData();
+  }, []);
+  
+
+
     return (
         <div>
+          {loading? (<LoadingBox></LoadingBox>)
+          :
+          error? (<MessageBox variant="danger">{error}</MessageBox>)
+          :
+          (
           <div className="row center">
-            {data.products.map((product) => (
+            {products.map((product) => (
               <Product key={product._id} product={product}></Product> 
               
             ))}
-          </div>
+          </div>)
+          }
+          
         </div>
     )
 }
